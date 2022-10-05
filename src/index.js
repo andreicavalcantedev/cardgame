063fdb4426d27509bf10ed50a0fb0bbf5f03149b
Andrei Cavalcante <andreisilva96832@gmail.com>
2022-10-05 17:53:35 -0300

    Feature: Adding Hook GetList/ Method: GET (Needs Install Axios to use that Hook)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CardGame from './pages/CardGame';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode> </React.StrictMode>
+  <React.StrictMode> <CardGame/> </React.StrictMode>
 );
